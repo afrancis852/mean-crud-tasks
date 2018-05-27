f506efa9ae0a171fe068d1495632e6ae559e71f7
afrancis <albert.francis852@gmail.com>
2018-05-27 17:27:03 -0300

    linked db

--- src/server/config.js
+++ src/server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
 	port: process.env.PORT || 8080,
-	db: process.env.MONGODB || 'mongodb://afrancis:blabla9090@ds235840.mlab.com:35840/dbtasks'
+	db: process.env.MONGODB || 'mongodb://user:12345@ds235840.mlab.com:35840/dbtasks'
 };
